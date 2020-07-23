4b00e7c1941f760d8ac15f3a15272b9e8c61fc8f
PPPPPrerna <pandeyprerna248@gmail.com>
2020-07-23 17:03:38 +0530

    first commit

---
+++ src/main/java/pack/Mavend/App.java
@@ -0,0 +1,13 @@
+package pack.Mavend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pack/Mavend/UisTest.java
@@ -0,0 +1,11 @@
+package pack.Mavend;
+
+import org.testng.annotations.Test;
+
+public class UisTest {
+
+	@Test
+	public void udse() {
+		System.out.println("Welcome");
+	}
+}
